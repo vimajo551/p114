9a1099a6b377cf69d910dfab0f86f7053e626c27
vimajo551 <104533881+vimajo551@users.noreply.github.com>
2022-12-16 20:38:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function preload(){
+var getNoseX
+var getNoseY
+var getHat
 
+function preload(){
+    getHat = loadImage("https://i.postimg.cc/76QY89g1/hat.png")
 }
 
 function setup(){
@@ -7,12 +11,29 @@ function setup(){
     canvas.position(712,150)
     video = createCapture(VIDEO)
     video.hide()
-    var myTextArea = createElement('textarea');  
-    myTextArea.attribute("rows","10");
-    myTextArea.attribute("cols","75");
-    myTextArea.position(57)
+    myTextArea = createElement('textarea');  
+    myTextArea.attribute("rows","14");
+    myTextArea.attribute("cols","44");
+    myTextArea.position(57);
+    myTextArea.style('background','red')
+    myTextArea.style('fontSize',23)
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        //console.log(results)
+        getNoseX=results[0].pose.nose.x-125
+        getNoseY=results[0].pose.nose.y-250
+    }
+}
+
+function modelLoaded(){
+    console.log("modelo carregado")
 }
 
 function draw(){
     image(video,0,0,550,450)
+    image(getHat,getNoseX,getNoseY,200,200)
 }
